dc9d447eaa0ec7a8242860d8499b3cb6a2b1f67c
Qilong Zhang <30571468+qilong-zhang@users.noreply.github.com>
2020-06-18 17:09:10 +0800

    Update search.py

--- search.py
+++ search.py
@@ -19,6 +19,10 @@ std = [0.229, 0.224, 0.225]
 # std = [1.0, 1.0, 1.0]
 net = torch.nn.Sequential(Normalize(mean, std), models.inception_v3(pretrained=True).eval()).cuda()
 
+transform = transforms.Compose([
+    transforms.Resize((330, 330)),
+    transforms.CenterCrop(299),
+    transforms.ToTensor()])
 
 
 print('loader data')
@@ -34,11 +38,7 @@ X = ImagetNet('../data/ILSVRC2012_train/pick_image/', 1000, 10, transforms = tra
 #         pin_memory=True)
 
 val_loader = torch.utils.data.DataLoader(
-    ImageFolder(testing_data_path, transforms.Compose([
-        transforms.Resize((330, 330)),
-        transforms.CenterCrop(299),
-        transforms.ToTensor(),
-        ])),
+    ImageFolder(testing_data_path, transforms = transform,
         batch_size = 50, shuffle=False,
         num_workers = 8, pin_memory=True)
 
